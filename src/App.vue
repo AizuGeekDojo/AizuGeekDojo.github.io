eae0493e98cee130d26b574dd679410a5d7f44a2
nozomi0966 <nozomotoitech@gmail.com>
2018-11-14 21:40:10 +0900

    add calender

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Calender />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Calender from "./components/calender.vue";
+import Header from "./components/header.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Calender,
+    Header
   }
 };
 </script>
@@ -23,6 +25,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="header">
+    <header>
+      <h1>GeekDojo WebPage</h1>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
+
+<style>
+header {
+  max-height: 200px;
+  max-width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: aquamarine;
+}
+
+@media only screen and (max-width: 600px) {
+  header {
+    height: 50px;
+  }
+}
+</style>
