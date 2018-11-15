f68aca57f586c8dff306f4114fb3caf775ddba63
nozomi0966 <nozomotoitech@gmail.com>
2018-11-15 11:33:58 +0900

    add publish page

---
+++ js/about.78f72aaf.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var u=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("Under Construct")])])}],o=n("2877"),r={},a=Object(o["a"])(r,u,s,!1,null,null,null);a.options.__file="About.vue";e["default"]=a.exports}}]);
+//# sourceMappingURL=about.78f72aaf.js.map
