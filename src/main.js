2beaa0f9d1ace8c840384b1de19b7e91dc94a1a3
nozomi0966 <nozomotoitech@gmail.com>
2018-11-15 12:14:32 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueAnalytics from "vue-analytics";
+// import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 
-new Vue(VueAnalytics, {
-  id: "UA-129319152-1",
+// new Vue(VueAnalytics, {
+//   id: "UA-129319152-1",
+//   router,
+//   render: h => h(App)
+// }).$mount("#app");
+
+new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
